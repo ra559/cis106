ed4ce6750895146e3a12ac804df14ceaf2b93883
ra559 <arobertadrian@gmail.com>
2021-01-13 14:57:31 -0500

    Task: Added oneliner

---
+++ guides/Cloud-Ubuntu-Setup-CIS-106.md
@@ -0,0 +1,64 @@
+# Ubuntu Setup Cloud Evn.
+
+# Virtual Machine Specifications
+* OS: Ubuntu 20.04 64 Bits [ISO URL](https://ubuntu.com/download/desktop) 
+* HDD: 50 GB
+* RAM: 4GB
+* Video: 128 MB or Higher
+* Audio Controller: Disabled (if possible)
+* CPU: 2 Cores
+* Hostname: cis106vm
+* User: student
+* Password: pccc
+* Shared Clipboard: Enabled - Bidirectional (For Virtualbox setting only N/A cloud) 
+
+
+# During Installation Settings:
+* Keyboard layout: English US
+* Installation type: Normal with third party software, Erase disk 
+* Timezone: NY
+* User Details
+  * Name: Student
+  * Hostname: cis106vm
+  * Username: Student
+  * Password: pccc
+  * Require login (Important)
+
+# Post Installation Settings:
+* Update and upgrade
+* Remove unnecessary software
+* Essential software install
+* Disable animations
+* Change Default File Manager: Nemo
+* Change Default screenshot tool with flameshot
+* Enable Dark Theme
+* One liner to complete this task:
+ 
+# VS Code setup
+1. Download deb package from: *https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64*
+2. Run this command (assuming that you downloaded the file to your Downloads directory):
+`sudo apt install $HOME/Downloads/code*.deb`
+3. Install VS Code Extensions:
+   1.  Live Server
+   2.  Markdown All in one
+   3.  Markdown Preview Enhanced
+   4.  Markdown PDF
+   5.  Markdown TOC
+   6.  PDF Preview
+   7.  Code Spell Checker
+
+# Change Desktop Environment
+* Default Desktop Environment: Gnome Flashback
+
+
+
+# VS Code Extentions:
+   1.  Live Server
+   2.  Markdown All in one
+   3.  Markdown Preview Enhanced
+   4.  Markdown PDF
+   5.  Markdown TOC
+   6.  PDF Preview
+   7.  Code Spell Checker
+
+
---
+++ guides/oneliner.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Update and upgrade
+sudo apt update; sudo apt upgrade -y
+
+# Remove unncessary software: 
+sudo apt purge apport aisleriot gnome-mahjongg gnome-sudoku gnome-mines thunderbird transmission-common  -y
+
+# Add essential software:
+sudo apt install gcc make perl gnome-session-flashback nemo ubuntu-restricted-extras \
+ubuntu-restricted-addons git python3-pip flameshot pandoc vlc \
+gnome-tweaks curl snapd flatpak gnome-software-plugin-flatpak \
+gnome-software-plugin-snap wget tree htop net-tools vlc peek \
+vim deluge geany tilix neofetch caffeine samba smbclient cifs-utils -y
+
+#Replace nautilus with nemo: 
+xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
+
+# Disable animations:
+gsettings set org.gnome.desktop.interface enable-animations false
+
+#Dark theme:
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
+
+#Replace default screenshot tool with flameshot (credit to: https://askubuntu.com/users/950843/philipp) (og: https://askubuntu.com/questions/1036473/ubuntu-18-how-to-change-screenshot-application-to-flameshot)
+gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot '[]'
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'flameshot'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command '/usr/bin/flameshot gui'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding 'Print'
