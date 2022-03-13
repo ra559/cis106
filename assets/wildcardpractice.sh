a7600c11bb493c966063a672f0a69d6b01351a3d
ra559 <arobertadrian@gmail.com>
2022-03-13 19:03:42 -0400

    Task: quick push

--- assets/wildcardpractice.sh
+++ assets/wildcardpractice.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir $HOME/wildcardpractice/
 cd $HOME/wildcardpractice/
-curl http://metaphorpsum.com/sentences/8 > randomtext.txt
-curl http://metaphorpsum.com/sentences/5 > random_text.md
-curl http://cis106.com/assets/cities.csv > _random.csv
-wget http://cis106.com/assets/sample.pdf
-wget http://cis106.com/assets/sample1.doc
-wget http://cis106.com/assets/sample1.xls
+curl -s http://metaphorpsum.com/sentences/8 > randomtext.txt
+curl -s http://metaphorpsum.com/sentences/5 > random_text.md
+curl -s http://cis106.com/assets/cities.csv > _random.csv
+wget -q http://cis106.com/assets/sample.pdf
+wget -q http://cis106.com/assets/sample1.doc
+wget -q http://cis106.com/assets/sample1.xls
 
 
