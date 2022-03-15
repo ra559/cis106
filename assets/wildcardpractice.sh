273aaa3b46ea3266d87d2644241ecb368cb402a8
ra559 <arobertadrian@gmail.com>
2022-03-14 22:28:20 -0400

    Task: quick push

--- assets/wildcardpractice.sh
+++ assets/wildcardpractice.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 mkdir $HOME/wildcardpractice/
 cd $HOME/wildcardpractice/
 curl -s http://metaphorpsum.com/sentences/8 > randomtext.txt
@@ -7,6 +7,7 @@ curl -s http://cis106.com/assets/cities.csv > _random.csv
 wget -q http://cis106.com/assets/sample.pdf
 wget -q http://cis106.com/assets/sample1.doc
 wget -q http://cis106.com/assets/sample1.xls
+
 # files for the ? wildcard
 tree -H $HOME > samplesite.html
 tree -J $HOME > samplejsonfile.json
@@ -14,6 +15,7 @@ echo "hiddenfile" > .hiddenFile.txt
 cp $HOME/.bash_history ./
 cp sample1.doc sample99.docx
 cp sample1.xls sample123.csv
+touch book.pdf bait.csv beat.exe fail.txt file.txt .hidden.doc .hidden2.js
 # Files for [] wildcard
 cp sample.pdf Sample.pdf
 cp randomtext.txt 78food.txt
