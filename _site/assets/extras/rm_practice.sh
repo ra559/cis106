c749e5b5edc5f48558aae5bd4dcd7404e0d37191
ra559 <arobertadrian@gmail.com>
2025-06-17 18:22:25 -0400

    remove site

--- _site/assets/extras/rm_practice.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p $HOME/OutdatedProjects/project{A..C}
-touch $HOME/OutdatedProjects/project{A..C}/old_data{1..3}.txt
-touch $HOME/OutdatedProjects/project{A..C}/notes.md
-touch $HOME/OutdatedProjects/project{A..C}/code.sh
-touch $HOME/OutdatedProjects/README.md
--- _site/assets/final_q2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir $HOME/final_q2
-cd $HOME/final_q2
-touch image{1..2}.{png,jpg,gif} 
-touch docs{1..2}.{pdf,docx} 
-touch program.{sh,rb,go,py}
-touch song{1..2}.{mp3,aac}
